6ee56e54d876f8641467e86b61f307dba6dd36fc
EC2 Default User <ec2-user@ip-172-31-83-76.ec2.internal>
2022-04-24 04:37:56 +0000

    [Finish]Bookers2

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,8 +9,8 @@ class User < ApplicationRecord
   has_one_attached :profile_image
 
   validates :name, presence: true, uniqueness: true, length: { minimum:2, maximum:20 }
-  validates :profile_image, presence: true
-  validates :introduction, presence: true, length: { maximum:50 }
+  #validates :profile_image, presence: true　空でも良かった
+  validates :introduction, length: { maximum:50 }#presence:trueいらなかった
 
   def get_profile_image(width, height)
   unless profile_image.attached?
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
+  get "home/about", to: "homes#about", as: :about
   root to: 'homes#top'
   devise_for :users
-
-  get "homes/about" => "homes#about"
   resources :users, only: [:index, :show, :edit, :update]
   resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
